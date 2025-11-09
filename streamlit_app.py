12d05cc2ec3e0c14677fe30a65b4b45471478340
RevathiN18 <revathinarayanan18@gmail.com>
2025-11-10 01:08:03 +0530

    Update streamlit_app.py
    smoothie froot response request added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+#streamlit.title('My Parents New Healthy Diner')
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie ! :cup_with_straw: ")
@@ -43,6 +44,11 @@ if ingredients_list:
 
     #st.write(my_insert_stmt)
     #st.stop()
+
+#New Section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 time_to_insert=st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
