27166e9e7ccf182588cf2d0089f726e5a3a0da06
RevathiN18 <revathinarayanan18@gmail.com>
2025-11-10 02:19:22 +0530

    Update streamlit_app.py
    error rectification

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
-#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+
 
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:'
@@ -56,6 +53,10 @@ if ingredients_list:
 
     #st.write(my_insert_stmt)
     #st.stop()
+#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 time_to_insert=st.button('Submit Order')
 if time_to_insert:
