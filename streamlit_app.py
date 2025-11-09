5931a500885c2ca2a739ae06e3872edc39d253cf
RevathiN18 <revathinarayanan18@gmail.com>
2025-11-10 01:11:30 +0530

    Update streamlit_app.py
    add json

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if ingredients_list:
 #New Section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 time_to_insert=st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
